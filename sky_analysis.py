353295e8279b33623c95621c6850463b7bb800ab
Ioana Zelko <ioana.zelko@gmail.com>
2021-06-08 15:18:14 -0400

    making the cepheus again

--- sky_analysis.py
+++ sky_analysis.py
@@ -601,9 +601,9 @@ if __name__ == "__main__":
     #run_list=["tiny_cepheus_beta_fixed2"]
     #run_list = ["tiny_cepheus_beta_varying2"]
     #run_list= ["tiny_cepheus_beta_fixed2_nside64"]
-    run_list=["tiny_cepheus_beta_fixed2_nside32" ]   
+    #run_list=["tiny_cepheus_beta_fixed2_nside32" ]   
 
-    #run_list= ["tiny_cepheus_beta_fixed_nside64"]
+    run_list= ["tiny_cepheus_beta_fixed_nside64"]
     #run_list=["tiny_cepheus_beta_fixed_nside32" ] 
     for run in run_list:
         print("Doing run ", run)
